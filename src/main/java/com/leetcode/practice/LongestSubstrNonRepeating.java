d7e6078c27ce50d57bd311ed132051220f2f839f
Mohitrajranu <mohitraj.ranu@gmail.com>
2021-10-28 21:07:39 +0530

    optimal solution

--- src/main/java/com/leetcode/practice/LongestSubstrNonRepeating.java
+++ src/main/java/com/leetcode/practice/LongestSubstrNonRepeating.java
@@ -1,9 +1,40 @@
 package com.leetcode.practice;
 
+
+import java.net.UnknownHostException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstrNonRepeating {
+
+    public int optlengthOfLongestSubstring(String s) {
+        if(s.length() <=1){
+            return s.length();
+        }
+
+        int longest=0;
+        Map<Character,Integer> seenChar= new HashMap<>();
+        int left=0;
+        for(int right=0;right<s.length();right++) {
+
+            Character ch = s.charAt(right);
+            if(seenChar.containsKey(ch)) {
+                int prevSeen = seenChar.get(ch);
+                if (prevSeen >= left) {
+                    left = prevSeen + 1;
+                }
+            }
+            seenChar.put(ch,right);
+            longest = Math.max(longest,right-left+1);
+
+
+        }
+
+
+        return longest;
+    }
 
     public int lengthOfLongestSubstring(String s) {
 
@@ -12,8 +43,6 @@ public class LongestSubstrNonRepeating {
         }
 
         int longest=0;
-
-
 
         for(int i=0;i<s.length();i++){
             int currentlength=0;
@@ -39,7 +68,7 @@ public class LongestSubstrNonRepeating {
     public static void main(String[] args) {
 
         LongestSubstrNonRepeating longestSubstrNonRepeating= new LongestSubstrNonRepeating();
-        System.out.println(longestSubstrNonRepeating.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(longestSubstrNonRepeating.optlengthOfLongestSubstring("abcabcbb"));
 
 
     }
---
+++ src/main/java/com/reflections/annotations/GettingClassObject.java
@@ -0,0 +1,37 @@
+package com.reflections.annotations;
+
+import java.lang.reflect.Constructor;
+
+class MyClass {
+   private MyClass(){
+        System.out.println("Instance Created");
+    }
+
+}
+public class GettingClassObject {
+
+    public static void main(String[] args) throws Exception {
+        //forName
+        Class<?> clss1= Class.forName("java.lang.String");
+        Class<?> clss2= Class.forName("java.lang.String");
+        System.out.println(clss1==clss2);
+        //ClassName.class
+        Class clss3= int.class;
+        Class<?> clss=Class.forName("com.reflections.annotations.MyClass");
+        Constructor<?> con=clss.getDeclaredConstructor();
+        con.setAccessible(true);
+        MyClass m=(MyClass)con.newInstance();
+
+       // MyClass m= new MyClass();
+        Class<? extends MyClass> class1= m.getClass();
+        //super class
+        Class<?> superclass= class1.getSuperclass();
+        //interfaces
+        Class<?>[] interfaces = class1.getInterfaces();
+        //getName
+        System.out.println(class1.getName());
+
+
+    }
+
+}
