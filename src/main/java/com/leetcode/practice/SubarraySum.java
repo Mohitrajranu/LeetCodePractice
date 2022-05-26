b718e52006281255a10ca2970636bfd1680b484a
Mohitrajranu <mohitraj.ranu@gmail.com>
2022-05-26 20:04:22 +0530

    ProblemSolving

--- src/main/java/com/leetcode/practice/SubarraySum.java
+++ src/main/java/com/leetcode/practice/SubarraySum.java
@@ -1,7 +1,9 @@
 package com.leetcode.practice;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class SubarraySum {
 
@@ -13,10 +15,28 @@ public class SubarraySum {
         for (int i = 0; i <nums.length ; i++) {
             prefixSum+=nums[i];
             ans+=mp.getOrDefault(prefixSum-k,0);
+
             mp.put(prefixSum,mp.getOrDefault(prefixSum,0)+1);
         }
 
 
         return ans;
+    }
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int sum = 0, prev = 0;
+        Set<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int temp = (k != 0) ? sum % k : sum;
+
+            if(set.contains(temp))
+                return true;
+
+            set.add(prev);
+            prev = temp;
+        }
+
+        return false;
     }
 }
