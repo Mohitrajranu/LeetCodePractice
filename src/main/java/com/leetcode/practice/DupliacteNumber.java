5803990660c36ac9d643c89b3a18fc8f1c98efa0
Mohitrajranu <mohitraj.ranu@gmail.com>
2025-01-26 17:57:03 +0530

    Optimize solution

--- src/main/java/com/leetcode/practice/DupliacteNumber.java
+++ src/main/java/com/leetcode/practice/DupliacteNumber.java
@@ -1,5 +1,11 @@
 package com.leetcode.practice;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class DupliacteNumber {
     public int findDuplicate(int[] nums) {
         if(nums.length<1){
@@ -23,5 +29,12 @@ public class DupliacteNumber {
 
         return hare;
 
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,2,4,5,4,6,1};
+        Set<Integer> duplicates = new HashSet<>();
+        List<Integer> dupNums = Arrays.stream(nums).filter(value -> !duplicates.add(value)).boxed().collect(Collectors.toList());
+        System.out.println(dupNums);
     }
 }
